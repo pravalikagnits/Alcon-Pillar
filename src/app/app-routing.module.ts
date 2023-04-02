b7ddbaeee1076478d9d2470ae5311e4add3bd487
IndrajithRam <indrajithcool@yahoo.com>
2023-04-01 23:36:49 -0400

    Login push 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { TournamentComponent } from './views/tournament/tournament.component';
+import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { title: 'Home' } },
   { path: 'tournamentsList', component: TournamentComponent, data: { title: 'Tournament' } },
+  { path: 'loginpage', component: LoginComponent, data: { title: 'Login' } },
   // { path: 'bookList', component: BookStoreComponent, data: { title: 'Book Store' } },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
